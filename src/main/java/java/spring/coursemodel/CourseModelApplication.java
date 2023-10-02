de5189fa3f2f5c545411892e3a6a6aa6dfda5729
dikshant <sabledikshant@gmail.com>
2023-10-02 17:22:52 +0530

    first commit

---
+++ src/main/java/java/spring/coursemodel/CourseModelApplication.java
@@ -0,0 +1,13 @@
+package java.spring.coursemodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseModelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java/spring/coursemodel/CourseModelApplicationTests.java
@@ -0,0 +1,13 @@
+package java.spring.coursemodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
